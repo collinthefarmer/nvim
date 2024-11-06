521057f7e3bca7dff7be23ffcf05d9875bde8005
Collin Farmer <collinthefarmer@gmail.com>
2024-11-06 17:53:13 -0500

    working on lsp stuff

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,62 @@
 return {
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+	{
+		"neovim/nvim-lspconfig",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"ms-jpq/coq_nvim",
+		dependencies = {
+			"ms-jpq/coq.artifacts",
+		},
+		init = function()
+			vim.g.coq_settings = {
+				auto_start = true,
+			}
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+			"ms-jpq/coq_nvim",
+		},
+		opts = {
+			-- add LSPs here...
+			ensure_installed = {
+				"astro",
+				"bashls",
+				"cssls",
+				"glsl_analyzer",
+				"golangci_lint_ls",
+				"html",
+				"jsonls",
+				"lua_ls",
+				"pyright",
+				"rust_analyzer",
+				"ts_ls",
+				"zls",
+			},
+			handlers = {
+				function (server)
+					require("lspconfig")[server].setup(require("coq").lsp_ensure_capabilities())
+				end,
+			}
+		},
+	},
 }
